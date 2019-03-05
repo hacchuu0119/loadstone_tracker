0b5c93efc8a1f2f7ca71b0d6eba40907635986ff
hacchuu0119 <kudokana33@gmail.com>
2019-03-05 20:02:48 +0900

    Allow space for comments

--- track_comment.sh
+++ track_comment.sh
@@ -16,7 +16,7 @@ done
 . $(pwd)/discord_util.sh
 new_response=$(curl $target_url | grep 'thread__comment__body' -B 16 | tail -17 | grep -e 'img src' -e thread__comment__body -e entry__name)
 new_comment_owner=$(echo $new_response | sed -e 's/^.*<p class="entry__name">//g' -e 's/<\/p>.*$//g' -e 's/ //g')
-new_comment=$(echo $new_response | sed -e 's/^.*<div class="thread__comment__body">//g' -e 's/<br \/>/\\n/g' -e 's/<\/div>//g')
+new_comment=$(echo $new_response | sed -e 's/^.*<div class="thread__comment__body">//g' -e 's/<br \/>/\\n/g' -e 's/<\/div>//g' -e 's/ //g')
 new_commwnt_pic=$(echo $new_response | sed -e 's/<img src="//g' -e 's/" alt="".*$//g')
 if [[ -n $(diff <(echo $new_comment) <( cat $last_response)) ]]; then
     echo $new_comment > $last_response
