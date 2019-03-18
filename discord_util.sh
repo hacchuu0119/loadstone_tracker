bb80f045af6125f109cc4b38ca4d2ceadb88200b
hacchuu0119 <38638059+hacchuu0119@users.noreply.github.com>
2019-03-19 02:48:18 +0900

    Update discord_util.sh
    hide discord url

--- discord_util.sh
+++ discord_util.sh
@@ -2,11 +2,10 @@ function discord_announce
 {
 json=$1
 
-default_webhookurl=$(echo https://discordapp.com/api/webhooks/537220101477498880/6TYCElUdnkSs3D8IpyAJyE2vFtpBmTohXaAGzXnjoyxMb1L2pk349BclgRGTz8NiVum5)
 default_username=$(echo kagarinII)
 #if [[ `ps aux | grep discordbot | wc | awk '{print $1}'`  < 2 ]];then
 
 webhookurl=$2
-[[ -z $webhookurl ]] && webhookurl=$default_webhookurl
+# [[ -z $webhookurl ]] && webhookurl=$default_webhookurl
 curl -H "Content-Type: application/json" -X POST --data $json $webhookurl
 }
